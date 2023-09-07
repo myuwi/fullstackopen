1ae5ffa2e16f6d07a3da22b1e8e5e8b80fb866b8
myuwi <myuwi@proton.me>
2023-09-07 11:59:08 +0300

    complete 1.2

--- part-01/courseinfo/src/App.jsx
+++ part-01/courseinfo/src/App.jsx
@@ -2,6 +2,14 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
 const Content = ({
   exercises1,
   exercises2,
@@ -11,17 +19,11 @@ const Content = ({
   part3,
 }) => {
   return (
-    <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-    </>
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </div>
   );
 };
 
