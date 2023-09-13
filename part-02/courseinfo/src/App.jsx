e480026d04e6cb56c08d60f70fbd81993c6c7988
myuwi <myuwi@proton.me>
2023-09-13 15:08:03 +0300

    complete 2.3

--- part-02/courseinfo/src/App.jsx
+++ part-02/courseinfo/src/App.jsx
@@ -21,7 +21,7 @@ const Content = ({ parts }) => {
 };
 
 const Total = ({ parts }) => {
-  const total = parts.reduce((acc, cur) => acc + cur.exercises, 0);
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
   return <strong>total of {total} exercises</strong>;
 };
 
