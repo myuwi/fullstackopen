64902ee729b02a53115ebbc68bf93fa62f39440b
myuwi <myuwi@proton.me>
2024-05-21 13:33:38 +0300

    complete 2.9

--- part-02/phonebook/src/App.jsx
+++ part-02/phonebook/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const filteredPersons = persons.filter((p) =>
+    p.name.toLowerCase().includes(filter.toLowerCase()),
+  );
 
   const addPerson = (e) => {
     e.preventDefault();
@@ -33,9 +36,17 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <h3>Add a new</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -47,8 +58,8 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map((person) => (
+      <h3>Numbers</h3>
+      {filteredPersons.map((person) => (
         <div key={person.name}>
           {person.name} {person.number}
         </div>
