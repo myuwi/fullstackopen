8958b1d25be3d32b6ff0241f6dcfddb063be7980
myuwi <myuwi@proton.me>
2024-05-23 18:33:24 +0300

    complete 3.20

--- part-03/phonebook/models/person.js
+++ part-03/phonebook/models/person.js
@@ -16,7 +16,12 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
+    minLength: 8,
     required: true,
+    validate: {
+      validator: (v) => /^\d{2,3}-\d+$/.test(v),
+      message: (props) => `${props.value} is not a valid phone number`,
+    },
   },
 });
 
