260a97d375ab8fe5b9d46b90c301b96bc142d72a
myuwi <myuwi@proton.me>
2024-05-24 15:14:14 +0300

    refactor tests

--- part-04/bloglist/tests/blog_api.test.js
+++ part-04/bloglist/tests/blog_api.test.js
@@ -1,5 +1,5 @@
 import assert from "node:assert";
-import { test, after, beforeEach } from "node:test";
+import { after, beforeEach, describe, test } from "node:test";
 import mongoose from "mongoose";
 import supertest from "supertest";
 
@@ -20,85 +20,89 @@ beforeEach(async () => {
 
 after(async () => await mongoose.connection.close());
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
-
-test("there are two blogs", async () => {
-  const res = await api.get("/api/blogs");
-  assert.strictEqual(res.body.length, 2);
-});
-
-test("blogs have an id", async () => {
-  const blogs = await helpers.blogsInDb();
-  blogs.forEach((blog) => {
-    assert(blog.id);
-    assert.strictEqual(blog._id, undefined);
+describe("when there is initially some blogs saved", () => {
+  test("blogs are returned as JSON", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
   });
-});
 
-test("a valid blog can be added", async () => {
-  const newBlog = helpers.blogs[2];
+  test("all blogs are returned", async () => {
+    const res = await api.get("/api/blogs");
+    assert.strictEqual(res.body.length, 2);
+  });
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+  test("all blogs have an id", async () => {
+    const blogs = await helpers.blogsInDb();
+    blogs.forEach((blog) => {
+      assert(blog.id);
+      assert.strictEqual(blog._id, undefined);
+    });
+  });
 
-  const blogsInDb = await helpers.blogsInDb();
+  describe("adding a new blog", () => {
+    test("succeeds when all fields are defined", async () => {
+      const newBlog = helpers.blogs[2];
 
-  assert.strictEqual(blogsInDb.length, initialBlogs.length + 1);
-  assert(blogsInDb.some((blog) => blog.title === newBlog.title));
-});
+      await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
 
-test("blog likes default to 0 when missing", async () => {
-  const newBlog = {
-    title: "A blog with 0 likes",
-    author: "Unpopular Author",
-    url: "localhost",
-  };
+      const blogsInDb = await helpers.blogsInDb();
 
-  const res = await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+      assert.strictEqual(blogsInDb.length, initialBlogs.length + 1);
+      assert(blogsInDb.some((blog) => blog.title === newBlog.title));
+    });
 
-  assert.strictEqual(res.body.likes, 0);
-});
+    test("succeeds when likes isn't defined, defaults to 0", async () => {
+      const newBlog = {
+        title: "A blog with 0 likes",
+        author: "Unpopular Author",
+        url: "localhost",
+      };
 
-test("blog title is required", async () => {
-  const newBlog = {
-    author: "Author",
-    url: "localhost",
-  };
+      const res = await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(400)
-    .expect("Content-Type", /application\/json/);
+      assert.strictEqual(res.body.likes, 0);
+    });
 
-  const blogsInDb = await helpers.blogsInDb();
-  assert.strictEqual(blogsInDb.length, initialBlogs.length);
-});
+    test("fails when title is missing", async () => {
+      const newBlog = {
+        author: "Author",
+        url: "localhost",
+      };
 
-test("blog url is required", async () => {
-  const newBlog = {
-    title: "Blog without an url",
-    author: "Author",
-  };
+      await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(400)
+        .expect("Content-Type", /application\/json/);
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(400)
-    .expect("Content-Type", /application\/json/);
+      const blogsInDb = await helpers.blogsInDb();
+      assert.strictEqual(blogsInDb.length, initialBlogs.length);
+    });
 
-  const blogsInDb = await helpers.blogsInDb();
-  assert.strictEqual(blogsInDb.length, initialBlogs.length);
+    test("fails when url is missing", async () => {
+      const newBlog = {
+        title: "Blog without an url",
+        author: "Author",
+      };
+
+      await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(400)
+        .expect("Content-Type", /application\/json/);
+
+      const blogsInDb = await helpers.blogsInDb();
+      assert.strictEqual(blogsInDb.length, initialBlogs.length);
+    });
+  });
 });
