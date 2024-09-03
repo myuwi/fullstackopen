05e743bd2edf289b4bd21b76077ab528dfaba2e2
myuwi <myuwi@proton.me>
2024-09-03 13:03:59 +0300

    complete 5.7

--- part-05/bloglist-frontend/src/components/Blog.jsx
+++ part-05/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,26 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [open, setOpen] = useState(false);
+  const toggleOpen = () => setOpen(!open);
+  const label = open ? "hide" : "view";
+
+  return (
+    <div className="blog">
+      <div>
+        {blog.title} {blog.author} <button onClick={toggleOpen}>{label}</button>
+      </div>
+      {open && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.user.name || blog.user.username}</div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
