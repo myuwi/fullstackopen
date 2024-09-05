d02e5218d467d8716f4b441e45e09adaaf1d86a1
myuwi <myuwi@proton.me>
2024-09-05 14:48:13 +0300

    complete 6.12

---
+++ part-06/redux-anecdotes/src/components/Notification.jsx
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux";
+
+const Notification = () => {
+  const notification = useSelector((state) => state.notification);
+
+  if (!notification) return null;
+  return <div className="notification">{notification}</div>;
+};
+
+export default Notification;
---
+++ part-06/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const notificationSlice = createSlice({
+  name: "notification",
+  initialState: null,
+});
+
+export default notificationSlice.reducer;
