7c51cfad8dd90d5ce305a75d6c490468db96ba56
myuwi <myuwi@proton.me>
2024-09-05 11:39:18 +0300

    complete 6.2

--- part-06/unicafe-redux/src/main.jsx
+++ part-06/unicafe-redux/src/main.jsx
@@ -7,21 +7,22 @@ import reducer from "./reducer";
 const store = createStore(reducer);
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: "GOOD",
-    });
-  };
+  const good = () => store.dispatch({ type: "GOOD" });
+  const ok = () => store.dispatch({ type: "OK" });
+  const bad = () => store.dispatch({ type: "BAD" });
+  const reset = () => store.dispatch({ type: "ZERO" });
+
+  const state = store.getState();
 
   return (
     <div>
       <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <button onClick={ok}>ok</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
+      <div>good {state.good}</div>
+      <div>ok {state.ok}</div>
+      <div>bad {state.bad}</div>
     </div>
   );
 };
