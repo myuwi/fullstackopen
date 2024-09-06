4d781afcd97d5b559dbc24300fc0cacea2ff262d
myuwi <myuwi@proton.me>
2024-09-06 10:13:16 +0300

    complete 7.1

---
+++ part-07/routed-anecdotes/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => (
+  <div>
+    Anecdote app for <a href="https://fullstackopen.com/">Full Stack Open</a>.
+    See{" "}
+    <a href="https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.jsx">
+      https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.jsx
+    </a>{" "}
+    for the source code.
+  </div>
+);
+
+export default Footer;
