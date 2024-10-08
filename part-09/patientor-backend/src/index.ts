10b97fa3e08827967d0adec344131abba9951ddc
myuwi <myuwi@proton.me>
2024-10-08 14:32:36 +0300

    complete 9.9

--- part-09/patientor-backend/src/index.ts
+++ part-09/patientor-backend/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 const app = express();
-app.use(express.json());
 
-const PORT = 3000;
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3001;
 
 app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
